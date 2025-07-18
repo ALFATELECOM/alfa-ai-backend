c7e3f19b59a2463bf82e77dce1fcd7ff3591eb74
ALFATELECOM <149483794+ALFATELECOM@users.noreply.github.com>
2025-07-18 23:18:57 +0530

    Delete app directory

--- app/routers/funds.py
+++
@@ -1,8 +0,0 @@
-
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/funds")
-async def get_funds(broker: str):
-    return {"status": "success", "broker": broker, "funds": 50000}
--- app/routers/orders.py
+++
@@ -1,8 +0,0 @@
-
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/orders")
-async def get_orders(broker: str):
-    return {"status": "success", "broker": broker, "orders": ["Order 1", "Order 2"]}
